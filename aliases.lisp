79904b32e289cd7ea2b1d18e41edecdb0e688871
Michael <michaelmalis2@gmail.com>
2014-04-20 14:12:41 -0500

    initial commit

---
+++ aliases.lisp
@@ -0,0 +1,19 @@
+(load "defalias.lisp")
+
+(defalias fn lambda)
+(defalias def defun)
+(defalias mac defmacro)
+; (defalias ++ incf) ; cannot use because ++ is already defined
+(defalias -- decf)
+
+;;; functions
+(defalias is eql)
+(defalias iso equalp)
+(defalias no not)
+(defalias mapf mapcar)
+(defalias isa typep)
+(defalias uniq gensym)
+(defalias even evenp)
+(defalias odd oddp)
+(defalias dedup remove-duplicates)
+(defalias table make-hash-table)
