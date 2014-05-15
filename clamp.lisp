d704f6efd5a492bdc714478e3161bc3990a53080
Michael <michaelmalis2@gmail.com>
2014-05-14 22:13:10 -0500

    Added some documentation and the each macro

--- clamp.lisp
+++ clamp.lisp
@@ -200,13 +200,22 @@
 
 ;;; iteration macros to avoid using loop directly
 (mac up (var a b &body body)
+  "Evaluates body iterating from a up to b inclusive"
   `(loop for ,var from ,a to ,b do ,@body))
 
 (mac down (var a b &body body)
+  "Evaluates body iterating from a down to b inclusive"
   `(loop for ,var from ,a downto ,b do ,@body))
 
 (mac while (test &body body)
+  "Evaluates body while the test is true"
   `(loop while ,test do ,@body))
 
 (mac until (test &body body)
+  "Evaluates body until the test is true"
   `(loop until ,test do ,@body))
+
+(mac each (var seq &body body)
+ "Iterates across each element in seq. Currently works
+  on both lists and arrays (maybe add hashtables in the future)"
+ `(loop for ,var being the elements of ,seq do ,@body))
