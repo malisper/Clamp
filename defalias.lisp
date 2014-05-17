7ac7d81a9431d4cefd266ecb62ad41d2cce41377
Michael <michaelmalis2@gmail.com>
2014-05-17 17:22:46 -0500

    changed the order of the functions so defalias is defined after the necessary functions

--- defalias.lisp
+++ defalias.lisp
@@ -1,12 +1,3 @@
-(defmacro defalias (new old)
-  "Makes new the same as old"
-  (cond ((macrop old)
-	   (make-macro new old))
-	((fnp old)
-	   (make-fn new old))
-	('else
-	  (error "Don't know what to do for object ~A of type ~A" old (type-of old)))))
-
 (defun macrop (x)
   "A predicate for testing whether x should be replaced with a macro"
   (and (symbolp x)
@@ -28,4 +19,13 @@
   "Generates the code for making new and old the same function"
   `(setf (documentation ',new 'function) (documentation ',old 'function)
 	 (symbol-function ',new) (symbol-function ',old)))
+
+(defmacro defalias (new old)
+  "Makes new the same as old"
+  (cond ((macrop old)
+	   (make-macro new old))
+	((fnp old)
+	   (make-fn new old))
+	('else
+	  (error "Don't know what to do for object ~A of type ~A" old (type-of old)))))
 
