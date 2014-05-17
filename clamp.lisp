475b5682111bdbf47813a08b6ba351ce364f57e6
Michael <michaelmalis2@gmail.com>
2014-05-17 09:46:39 -0500

    modified the iteration constructs so loop does not leak through
    Previously (up i 1 10 (print i) collect i) would expand into
    (loop for i from 1 to 10 do (print i) collect i). Now the code
    is no longer valid.

--- clamp.lisp
+++ clamp.lisp
@@ -201,21 +201,21 @@
 ;;; iteration macros to avoid using loop directly
 (mac up (var a b &body body)
   "Evaluates body iterating from a up to b inclusive"
-  `(loop for ,var from ,a to ,b do ,@body))
+  `(loop for ,var from ,a to ,b do (progn ,@body)))
 
 (mac down (var a b &body body)
   "Evaluates body iterating from a down to b inclusive"
-  `(loop for ,var from ,a downto ,b do ,@body))
+  `(loop for ,var from ,a downto ,b do (progn ,@body)))
 
 (mac while (test &body body)
   "Evaluates body while the test is true"
-  `(loop while ,test do ,@body))
+  `(loop while ,test do (progn ,@body)))
 
 (mac until (test &body body)
   "Evaluates body until the test is true"
-  `(loop until ,test do ,@body))
+  `(loop until ,test do (progn ,@body)))
 
 (mac each (var seq &body body)
  "Iterates across each element in seq. Currently works
   on both lists and arrays (maybe add hashtables in the future)"
- `(loop for ,var being the elements of ,seq do ,@body))
+ `(loop for ,var being the elements of ,seq do (progn ,@body)))
