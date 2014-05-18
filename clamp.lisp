e2d982329b2aff6d63ad4425ea0bc7fa8c56cd4a
Michael <michaelmalis2@gmail.com>
2014-05-18 16:37:52 -0500

    added documentation for pr and prn

--- clamp.lisp
+++ clamp.lisp
@@ -266,9 +266,13 @@
 
 ;;; printing functins
 (def pr (&rest args)
+  "Prints all of its arguments to *standard-output* in a human
+   readable format"
   (mapf #'princ args)
   (car args))
 
 (def prn (&rest args)
+  "Prints all of its arguments to *standard-output* in a human
+   readable format with an additional newline"
   (prog1 (apply #'pr args)
          (terpri)))
