f24396e06325e177d677501c1a8f8ace2c15613e
Michael <michaelmalis2@gmail.com>
2014-05-23 16:04:21 -0500

    initial commit

---
+++ src/fns.lisp
@@ -0,0 +1,10 @@
+;;;; These are macros which allow allow for different kinds of fns
+
+(mac rfn (name parms &body body)
+  "Creates a recursive function which can refer to itself through name"
+  `(labels ((,name ,parms ,@body))
+     #',name))
+
+(mac afn (parms &body body)
+  "Creates a recursive function which can refer to itself through 'self'"
+  `(rfn self ,parms ,@body))
