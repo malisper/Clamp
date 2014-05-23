655cc9c4dfa01aac2715b275fc792bade5f9d615
Michael <michaelmalis2@gmail.com>
2014-05-23 16:36:26 -0500

    replaced mac with defmacro

--- src/conditions.lisp
+++ src/conditions.lisp
@@ -1,10 +1,10 @@
 ;;;; these are different kinds of conditionals
 
-(mac lf (&body rest)
+(defmacro lf (&body rest)
   "Cond but doesn't require parens for each clause"
   `(cond ,@(pair rest)))
 
-(mac check (x test &optional alt)
+(defmacro check (x test &optional alt)
   "If x passes the test, otherwise evaluate alt"
   (w/uniq gx
     `(let1 ,gx ,x
@@ -12,7 +12,7 @@
 	   ,gx
 	   ,alt))))
 
-(mac lflet (var expr &body branches)
+(defmacro lflet (var expr &body branches)
   "Same as lf but if a predicate is true, it is bound to var"
   (lf branches
       (w/uniq gv
@@ -25,19 +25,19 @@
 		    `(lflet ,var ,@(cdr branches))))))
       expr))
 
-(mac whenlet (var expr &body body)
+(defmacro whenlet (var expr &body body)
   "Analog of lflet but for when"
   `(lflet ,var ,expr (progn ,@body)))
 
-(mac alf (expr &body branches)
+(defmacro alf (expr &body branches)
    "lflet but uses 'it' for var"
   `(lflet it ,expr ,@branches))
 
-(mac awhen (expr &body body)
+(defmacro awhen (expr &body body)
    "Analog of alf but for when"
   `(whenlet it ,expr ,@body))
 
-(mac aand (&rest args)
+(defmacro aand (&rest args)
   "Evaluates each argument one by one. Binds the result of the previous
    expression to 'it'. Otherwise the same as and"
   (lf (no args)
