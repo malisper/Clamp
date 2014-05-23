62dcce4899de27b5863cf55def3146dbacc4a4a1
Michael <michaelmalis2@gmail.com>
2014-05-23 16:57:56 -0500

    modified accum and check to use new mac

--- src/misc.lisp
+++ src/misc.lisp
@@ -19,20 +19,18 @@
 
 (mac accum (accfn &body body)
   "The result is all of the arguments passed into accfn"
-  (w/uniq (gacc garg)
-    `(let1 ,gacc '()
-       (flet1 ,accfn (,garg) (push ,garg ,gacc) ; uniq for garg seemingly not required but not taking any risks
-	 ,@body)
-       (nrev ,gacc))))
+  `(let1 gacc@ '()
+     (flet1 ,accfn (garg) (push garg gacc@) ; uniq for garg seemingly not required but not taking any risks
+       ,@body)
+     (nrev gacc@)))
 
 (def multiple (x y)
   "A predicate for testing if x is a multiple of y"
   (zerop (mod x y)))
 
-(defmacro check (x test &optional alt)
+(mac check (x test &optional alt)
   "If x passes the test, otherwise evaluate alt"
-  (w/uniq gx
-    `(let1 ,gx ,x
-       (lf (funcall ,test ,gx)
-	   ,gx
-	   ,alt))))
+  `(let1 x@ ,x
+     (lf (funcall ,test x@)
+	 x@
+	 ,alt)))
