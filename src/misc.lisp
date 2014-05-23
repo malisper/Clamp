9467ef521c2533d2717b0682ef5112c4c2a3b625
Michael <michaelmalis2@gmail.com>
2014-05-23 16:24:47 -0500

    initial commit

---
+++ src/misc.lisp
@@ -0,0 +1,30 @@
+;;;; utilities which do not belong in any other file
+
+(def range (a b)
+  "Generates the range of numbers from a to b"
+  (lf (> a b)
+      '()
+      (cons a (range (1+ a) b))))
+
+(mac ado (&body body)
+  "Evaluates each expression with it bound to the result of
+   the previous one. Returns the value of the last expression"
+  (lf (null body)
+        nil
+      (single body)
+        (car body)
+      `(let1 it ,(car body)
+	(declare (ignorable it))
+        (ado ,@(cdr body)))))
+
+(mac accum (accfn &body body)
+  "The result is all of the arguments passed into accfn"
+  (w/uniq (gacc garg)
+    `(let1 ,gacc '()
+       (flet1 ,accfn (,garg) (push ,garg ,gacc) ; uniq for garg seemingly not required but not taking any risks
+	 ,@body)
+       (nrev ,gacc))))
+
+(def multiple (x y)
+  "A predicate for testing if x is a multiple of y"
+  (zerop (mod x y)))
