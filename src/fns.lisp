ca19eae380ba0f04949d7f312781793dc7562aa1
Michael <michaelmalis2@gmail.com>
2014-05-23 22:15:19 -0500

    fixed typo

--- src/fns.lisp
+++ src/fns.lisp
@@ -1,4 +1,4 @@
-;;;; These are defmacroros which allow allow for different kinds of fns
+;;;; These are macros which allow allow for different kinds of fns
 
 (defmacro rfn (name parms &body body)
   "Creates a recursive function which can refer to itself through name"
