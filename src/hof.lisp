fd98b13504829f6227a11736572c6acbcd7bf825
Michael <michaelmalis2@gmail.com>
2014-06-02 13:17:50 -0500

    added cnt and changed documentation strings

--- src/hof.lisp
+++ src/hof.lisp
@@ -6,26 +6,27 @@
   (if (functionp x) x [iso x _]))
 
 (def del (test xs &rest args)
-  "Deletes all of the elements that pass test in the list xs. If test
-   is a function all objects passing the test are removed. If test is
-   anything else, all objects iso with it are removed
-   NOTE: note the same as delete, as del is not a destructive function"
+  "Version of remove that uses testify"
   (apply #'remove-if (testify test) xs args))
 
 (def keep (test xs &rest args)
-  "Same as del but keeps the elements that pass the test"
+  "Version of remove-if-not that uses testify"
   (apply #'remove-if-not (testify test) xs args))
 
 (def mem (test xs &rest args)
-  "Analog for del and keep but for member"
+  "Version of mem that uses testify"
   (apply #'member-if (testify test) xs args))
 
 (def fnd (test xs &rest args)
-  "Analog for del and keep but for find"
+  "Version of find that uses testify"
   (apply #'find-if (testify test) xs args))
 
+(def cnt (test xs &rest args)
+  "Version of count that uses testify"
+  (apply #'count-if (testify test) xs args))
+
 (def pos (test xs &rest args)
-  "Analog for del and keep but for position"
+  "Version of position that uses testify"
   (apply #'position-if (testify test) xs args))
 
 (def mappend (f &rest xss)
