001aecbe0dc71ed6f8261820fd558958d4ad76b6
Michael <michaelmalis2@gmail.com>
2014-06-08 14:58:46 -0500

    renamed let1 let, over the normal cl let

--- src/binding.lisp
+++ src/binding.lisp
@@ -1,19 +1,21 @@
 ;;;; these are macros which create new bindings
 
+(in-package "CLAMP")
+
 (defmacro with (parms &body body)
   "Destructuring-bind (let) but doesn't require parens for each binding"
   (let* ((pparms (pair parms))
-	 (pats (mapf #'car  pparms))
-	 (vals (mapf #'cadr pparms)))
+	 (pats (map #'car  pparms))
+	 (vals (map #'cadr pparms)))
     `(destructuring-bind ,pats (list ,@vals) ,@body)))
 
-(defmacro let1 (var val &body body)
+(defmacro let (var val &body body)
   "With but with only one variable (no parens)"
   `(with (,var ,val) ,@body))
 
 (defmacro ret (var val &body body)
-  "Let1 but the result of the expression is the final value of the variable"
-  `(let1 ,var ,val ,@body ,var))
+  "Let but the result of the expression is the final value of the variable"
+  `(let ,var ,val ,@body ,var))
 
 (defmacro flet1 (name args fbody &body body)
   "Flet but with only one binding (no parens)"
@@ -22,6 +24,6 @@
 (defmacro withs (parms &body body)
   "With analog for let* but doesn't require parens around each binding"
   (if (no parms)
-      `(progn ,@body)
-      `(let1 ,(car parms) ,(cadr parms)
+      `(do ,@body)
+      `(let ,(car parms) ,(cadr parms)
 	 (withs ,(cddr parms) ,@body))))
