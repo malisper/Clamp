27ee2dbef50385b44115bbaaaab04d6e3ad50633
Michael <michaelmalis2@gmail.com>
2014-06-08 15:03:23 -0500

    added packages. Also created new sort which puts the predicate first and calls regular cl:sort

--- src/sort.lisp
+++ src/sort.lisp
@@ -1,5 +1,7 @@
 ;;;; functions for specific kinds of sorting
 
+(in-package "CLAMP")
+
 (def compare (comparer scorer)
   "Returns a function which compares its arguments score on scorer
    with comparer. Generally should use the :key argument to other
@@ -8,12 +10,16 @@
 
 (def best (f seq)
   "Finds the first element of seq if it was sorted using f"
-  (lf (no seq)
+  (if (no seq)
       nil
       (ret wins (car seq)
 	(each elt (cdr seq)
-	  (lf (funcall f elt wins) (setf wins elt))))))
+	  (if (funcall f elt wins) (setf wins elt))))))
 
 (def bestn (n f seq)
   "Finds the first n elements of seq if it was sorted using f"
   (firstn n (sort seq f)))
+
+(def sort (comparer sequence &optional (key #'identity))
+  "Non-destructively sorts the sequence using comparer"
+  (cl:sort sequence comparer :key key))
