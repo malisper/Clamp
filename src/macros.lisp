a9d8bc45a60fd0642297e1acc168bd208d3d5c0e
Michael <michaelmalis2@gmail.com>
2014-06-08 15:01:46 -0500

    added packages

--- src/macros.lisp
+++ src/macros.lisp
@@ -1,19 +1,21 @@
 ;;;; macros for writing macros
 
+(in-package "CLAMP")
+
 (defmacro mac (name args &body body)
   "The same as defmacro except allows auto-uniq ie any symbol that ends with
    '@' will be replaced by a uniq symbol"
-  (let1 uniqs (table)
+  (let uniqs (table)
     `(defmacro ,name ,args
-       ,@(alter #'auto
+       ,@(subst #'auto
 		[or (gethash _ uniqs) (setf (gethash _ uniqs) (uniq (symbol-name _)))]
 		body))))
 
 (defmacro w/uniq (names &body body)
   "Binds each element in names (or names if it is just a symbol), with
    a unique symbol"
-  (lf (consp names)
+  (if (consp names)
       `(with ,(mappend (fn (n) `(,n (uniq (symbol-name ',n))))
 		       names)
 	 ,@body)
-      `(let1 ,names (uniq (symbol-name ',names)) ,@body)))
+      `(let ,names (uniq (symbol-name ',names)) ,@body)))
