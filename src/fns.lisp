406ddf49f6560bada2675acb1c8ff9b1ce78e9a9
Michael <michaelmalis2@gmail.com>
2014-06-08 15:02:10 -0500

    added packages

--- src/fns.lisp
+++ src/fns.lisp
@@ -1,5 +1,7 @@
 ;;;; These are macros which allow allow for different kinds of fns
 
+(in-package "CLAMP")
+
 (defmacro rfn (name parms &body body)
   "Creates a recursive function which can refer to itself through name"
   `(labels ((,name ,parms ,@body))
