3b9815454e728f31c3bddacdc63ac91771976cf0
Michael <michaelmalis2@gmail.com>
2014-06-08 14:59:29 -0500

    added packages

--- src/print.lisp
+++ src/print.lisp
@@ -1,13 +1,15 @@
 ;;;; functions for printing
 
+(in-package "CLAMP")
+
 (def pr (&rest args)
   "Prints all of its arguments to *standard-output* in a human
    readable format"
-  (mapf #'princ args)
+  (map #'princ args)
   (car args))
 
 (def prn (&rest args)
   "Prints all of its arguments to *standard-output* in a human
    readable format with an additional newline"
-  (prog1 (apply #'pr args)
-         (terpri)))
+  (do1 (apply #'pr args)
+       (terpri)))
