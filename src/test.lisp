dbc8634f2f974a67b24ab949b814f9dc80350f4e
Michael <michaelmalis2@gmail.com>
2014-06-12 22:24:23 -0500

    added tests for binding

--- src/test.lisp
+++ src/test.lisp
@@ -4,8 +4,9 @@
 (clunit:defsuite clamp ())
 
 (clunit:defsuite base (clamp))
+(clunit:defsuite binding (clamp))
 
-;;; figure out how to add test for reader macros
+;;; figure out how to add tests for reader macros
 
 (clunit:deftest single (base)
   (clunit:assert-true  (single '(a)))
@@ -26,3 +27,24 @@
 (clunit:deftest if (base)
   (clunit:assert-expands (cond (a b) (c d)) (if a b c d))
   (clunit:assert-expands (cond (a b) (t c)) (if a b c)))
+
+(clunit:deftest with (binding)
+  (clunit:assert-eql (with (a 1 b 2 c 3) (+ a b c)) 6)
+  (clunit:assert-eql (with ((x y) (list 1 2) z 3) (+ x y z)) 6)
+  (clunit:assert-equal (with (a 1 b 2) (with (a b b a) (list a b)))
+                       '(2 1)))
+
+(clunit:deftest let (binding)
+  (clunit:assert-expands (with (a b) c) (let a b c))
+  (clunit:assert-eql (let x 3 (+ x 5)) 8)
+  (clunit:assert-eql (let (x . y) (cons 1 2) (+ x y)) 3))
+
+(clunit:deftest ret (binding)
+  (clunit:assert-eql (ret x 5 (incf x 10)) 15)
+  (clunit:assert-equal (ret x '() (push 'b x) (push 'a x)) '(a b)))
+
+(clunit:deftest flet1 (binding)
+  (clunit:assert-expands (flet ((a (x y z) b))) (flet1 a (x y z) b)))
+
+(clunit:deftest withs (binding)
+  (clunit:assert-eql (withs (x 5 y (+ x 3)) (+ y 4)) 12))
