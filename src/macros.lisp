75fb8a03232b25a16cedeaf7679762c519979d21
Michael <michaelmalis2@gmail.com>
2014-06-22 17:30:28 -0500

    stopped use of autouniq mac

--- src/macros.lisp
+++ src/macros.lisp
@@ -2,16 +2,20 @@
 
 (in-package "CLAMP")
 
-(defmacro mac (name args &body body)
-  "The same as defmacro except allows auto-uniq ie any symbol that ends with
-   '@' will be replaced by a uniq symbol"
-  (let uniqs (table)
-    `(defmacro ,name ,args
-       ,@(subst #'auto
-		[or (gethash _ uniqs) (= (gethash _ uniqs) (uniq (symbol-name _)))]
-		body))))
+;;; Does not work because the substitution of the autouniqs only happens
+;;; once when the macro is generated. Not each time the macro is executed
+;;; like they should be.
 
-(defmacro w/uniq (names &body body)
+;; (defmacro mac (name args &body body)
+;;   "The same as defmacro except allows auto-uniq ie any symbol that ends with
+;;    '@' will be replaced by a uniq symbol"
+;;   (let uniqs (table)
+;;     `(defmacro ,name ,args
+;;        ,@(subst #'auto
+;; 		[or (gethash _ uniqs) (= (gethash _ uniqs) (uniq (symbol-name _)))]
+;; 		body))))
+
+(mac w/uniq (names &body body)
   "Binds each element in names (or names if it is just a symbol), with
    a unique symbol"
   (if (consp names)
