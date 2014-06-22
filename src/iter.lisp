8cc9720966f5fb971ac38350eb144020a7a74569
Michael <michaelmalis2@gmail.com>
2014-06-22 17:28:44 -0500

    defmacro -> mac

--- src/iter.lisp
+++ src/iter.lisp
@@ -2,32 +2,32 @@
 
 (in-package "CLAMP")
 
-(defmacro rec (withses &body body)
+(mac rec (withses &body body)
   "Same as loop in Anarki. Look for use cases"
   (let w (pair withses)
     `(funcall (rfn recur ,(map #'car w) ,@body) ,@(map #'cadr w))))
 
-(defmacro repeat (n &body body)
+(mac repeat (n &body body)
   "Excutes the body n times"
   `(loop repeat ,n do (do ,@body)))
 
-(defmacro up (var a b &body body)
+(mac up (var a b &body body)
   "Evaluates body iterating from a up to b inclusive"
   `(loop for ,var from ,a to ,b do (do ,@body)))
 
-(defmacro down (var a b &body body)
+(mac down (var a b &body body)
   "Evaluates body iterating from a down to b inclusive"
   `(loop for ,var from ,a downto ,b do (do ,@body)))
 
-(defmacro while (test &body body)
+(mac while (test &body body)
   "Evaluates body while the test is true"
   `(loop while ,test do (do ,@body)))
 
-(defmacro until (test &body body)
+(mac until (test &body body)
   "Evaluates body until the test is true"
   `(loop until ,test do (do ,@body)))
 
-(defmacro each (var seq &body body)
+(mac each (var seq &body body)
  "Iterates across each element in seq. Currently works
   on both lists and arrays (maybe add hashtables in the future)"
  `(loop for ,var being the elements of ,seq do (do ,@body)))
