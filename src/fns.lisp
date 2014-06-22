6d4094045942e757cd3da46a65a3895ef331b2b7
Michael <michaelmalis2@gmail.com>
2014-06-22 17:26:26 -0500

    defmacro -> mac

--- src/fns.lisp
+++ src/fns.lisp
@@ -2,11 +2,11 @@
 
 (in-package "CLAMP")
 
-(defmacro rfn (name parms &body body)
+(mac rfn (name parms &body body)
   "Creates a recursive function which can refer to itself through name"
   `(labels ((,name ,parms ,@body))
      #',name))
 
-(defmacro afn (parms &body body)
+(mac afn (parms &body body)
   "Creates a recursive function which can refer to itself through 'self'"
   `(rfn self ,parms ,@body))
