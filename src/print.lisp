4789dc8fdfad88b0e6ded63c0ae7603b6a8748c1
Michael <michaelmalis2@gmail.com>
2014-06-30 16:54:29 -0500

    added several macros for dealing with i/o

--- src/print.lisp
+++ src/print.lisp
@@ -13,3 +13,21 @@
    readable format with an additional newline"
   (do1 (apply #'pr args)
        (terpri)))
+
+(mac w/outstring (var &rest body)
+  "Creates a stream 'var' and returns all of the output to var"
+  `(with-output-to-string (,var)
+     ,@body))
+
+(mac tostring (&body body)
+  "Collects all of the output to *standard-output* into a string"
+  `(w/outstring *standard-output* ,@body))
+
+(mac w/instring (var string &rest body)
+  "Creates an input stream which reads from string"
+  `(with-input-from-string (,var ,string)
+     ,@body))
+
+(mac fromstring (string &body body)
+  "Binds the input from *standard-input to the string"
+  `(w/instring *standard-input* ,string ,@body))
