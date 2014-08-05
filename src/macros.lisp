665213de519c78db472c530040e8317d62002daa
Michael <michaelmalis2@gmail.com>
2014-08-04 23:54:56 -0500

    Removed broken implementation of autouniqs.
    I might just wind up using defmacro! from Let over Lambda

--- src/macros.lisp
+++ src/macros.lisp
@@ -2,19 +2,6 @@
 
 (in-package "CLAMP")
 
-;;; Does not work because the substitution of the autouniqs only happens
-;;; once when the macro is generated. Not each time the macro is executed
-;;; like they should be.
-
-;; (defmacro mac (name args &body body)
-;;   "The same as defmacro except allows auto-uniq ie any symbol that ends with
-;;    '@' will be replaced by a uniq symbol"
-;;   (let uniqs (table)
-;;     `(defmacro ,name ,args
-;;        ,@(subst #'auto
-;; 		[or (gethash _ uniqs) (= (gethash _ uniqs) (uniq (symbol-name _)))]
-;; 		body))))
-
 (mac w/uniq (names &body body)
   "Binds each element in names (or names if it is just a symbol), with
    a unique symbol"
