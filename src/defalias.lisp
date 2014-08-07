d218ebbab32b4370b0f65e8d5d73200f1ae39a20
Michael <michaelmalis2@gmail.com>
2014-08-07 17:21:34 -0500

    Now defalias will also copy the compiler-macro of a function.

--- src/defalias.lisp
+++ src/defalias.lisp
@@ -3,33 +3,37 @@
 (in-package "CLAMP")
 
 (defun macrop (x)
-  "A predicate for testing whether x should be replaced with a macro"
+  "Should x be replaced with a macro?"
   (and (symbolp x)
        (or (special-operator-p x)
 	   (macro-function x))))
 
 (defun make-macro (new old)
-  "Generates the code for making new and old the same macro"
+  "Generates the code for a macro 'new' which will expand into a call
+   to old."
   (cl:let ((rest (gensym "REST")))
-    `(progn (setf (documentation ',new 'function) (documentation ',old 'function))
+    `(progn (setf (documentation ',new 'function)
+		  (documentation ',old 'function))
 	    (defmacro ,new (&rest ,rest)
 	      `(,',old ,@,rest)))))
 
 (defun fnp (x)
-  "A predicate for testing whether x is a function"
+  "Is x a function?"
   (and (symbolp x) (symbol-function x)))
 
 (defun make-fn (new old)
-  "Generates the code for making new and old the same function"
+  "Generates the code for making new and old the same function."
   `(setf (documentation ',new 'function) (documentation ',old 'function)
-	 (symbol-function ',new) (symbol-function ',old)))
+	 (symbol-function ',new) (symbol-function ',old)
+	 (compiler-macro-function ',new) (compiler-macro-function ',old)))
 
 (defmacro defalias (new old)
-  "Makes new the same as old"
+  "Makes a use of new the equivalent to a use of old."
   (cond ((macrop old)
 	   (make-macro new old))
 	((fnp old)
 	   (make-fn new old))
 	('else
-	  (error "Don't know what to do for object ~A of type ~A" old (type-of old)))))
+	  (error "Don't know what to do for object ~A of type ~A"
+		 old (type-of old)))))
 
