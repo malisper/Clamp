b0c90644d0bb0e53dd9071f0c87a2849dff2428c
Michael <michaelmalis2@gmail.com>
2014-08-07 17:30:57 -0500

    Fixed bug in split, the first argument returned would always be a list.

--- src/list.lisp
+++ src/list.lisp
@@ -24,7 +24,7 @@
   "Given a sequence and an integer will return two sequences. The first
    one will contain the first n elements of the sequence, and the second
    will contain the rest of the elements of the initial sequence."
-  (values (firstn n seq) (cut seq n)))
+  (values (cut n seq) (cut seq n)))
 
 (def group (xs &key (by 2) (with #'list))
   "Groups every 'by' elements of the given list using the procedure 
