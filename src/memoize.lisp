f17205a5d416924d46caff45c0a4234fb3056208
Michael <michaelmalis2@gmail.com>
2014-08-12 18:34:41 -0500

    fixed indentation

--- src/memoize.lisp
+++ src/memoize.lisp
@@ -5,18 +5,18 @@
 (def memo (f)
   "Returns a memoized version of the function f"
   (let cache (table :test #'iso)
-		(fn (&rest args)
-			(aif2 (gethash args cache)
-						it
-						(= (gethash args cache)
-							 (apply f args))))))
+    (fn (&rest args)
+        (aif2 (gethash args cache)
+              it
+              (= (gethash args cache)
+                 (apply f args))))))
 
 (mac defmemo (name args &body body)
   "Defines a memoized function."
-	(let fn-body `(memo (fn ,args (block ,name ,@body)))
-		(w/uniq args
-			`(do (defun ,name (&rest ,args) (apply ,fn-body ,args))
-					 (= (symbol-function ',name) ,fn-body)
-				   ,(when (stringp (car body))
-									`(= (documentation ',name 'function) ,(car body)))
-				   ',name))))
+  (let fn-body `(memo (fn ,args (block ,name ,@body)))
+    (w/uniq args
+      `(do (defun ,name (&rest ,args) (apply ,fn-body ,args))
+           (= (symbol-function ',name) ,fn-body)
+           ,(when (stringp (car body))
+                  `(= (documentation ',name 'function) ,(car body)))
+           ',name))))
