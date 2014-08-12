3e57c226f9f88d56ce268ec14985cc357a860855
Michael <michaelmalis2@gmail.com>
2014-08-11 19:39:05 -0500

    Modified defalias
    It will now actually set the macro-function instead of creating a new
    macro. This allows slime to be able to have the lambda list so it can
    provide it.

--- src/defalias.lisp
+++ src/defalias.lisp
@@ -1,21 +1,17 @@
 ;;;; definitions for defalias which allows redefinition of macros/fns
 
-(in-package "CLAMP")
+(in-package :clamp)
 
 (defun macrop (x)
-  "Should x be replaced with a macro?"
-  (and (symbolp x)
-       (or (special-operator-p x)
-	   (macro-function x))))
+  "Is x a macro?"
+	(and (symbolp x) (macro-function x)))
 
 (defun make-macro (new old)
-  "Generates the code for a macro 'new' which will expand into a call
-   to old."
-  (cl:let ((rest (gensym "REST")))
-    `(progn (setf (documentation ',new 'function)
-		  (documentation ',old 'function))
-	    (defmacro ,new (&rest ,rest)
-	      `(,',old ,@,rest)))))
+  "Generates the code for making the macros new and old equivalent."
+  `(progn
+		 (defmacro ,new ())
+		 (setf (documentation ',new 'function) (documentation ',old 'function)
+					 (macro-function ',new) (macro-function ',old))))
 
 (defun fnp (x)
   "Is x a function?"
@@ -23,17 +19,29 @@
 
 (defun make-fn (new old)
   "Generates the code for making new and old the same function."
-  `(setf (documentation ',new 'function) (documentation ',old 'function)
-	 (symbol-function ',new) (symbol-function ',old)
-	 (compiler-macro-function ',new) (compiler-macro-function ',old)))
+	;; compiler macros may be risky if they rely on using the same
+	;; symbol they are named with so they are not included
+  `(progn
+		 (defun ,new ())
+		 (setf (documentation ',new 'function) (documentation ',old 'function)
+					 (symbol-function ',new) (symbol-function ',old))))
+
+(defun make-special-macro (new old)
+	"Generates the code to create a macro 'new' which expands into a use 
+   of the special form 'old'."
+	(cl:let ((rest (gensym "REST")))
+		`(defmacro ,new (&rest ,rest)
+			 `(,',old ,@,rest))))
 
 (defmacro defalias (new old)
   "Makes a use of new the equivalent to a use of old."
-  (cond ((macrop old)
-	   (make-macro new old))
-	((fnp old)
-	   (make-fn new old))
-	('else
-	  (error "Don't know what to do for object ~A of type ~A"
-		 old (type-of old)))))
+  (cond ((special-operator-p old)
+				 (make-special-macro new old))
+				((macrop old)
+				 (make-macro new old))
+				((fnp old)
+				 (make-fn new old))
+				(:else
+				 (error "Don't know what to do for object ~A of type ~A"
+								old (type-of old)))))
 
