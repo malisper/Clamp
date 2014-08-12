020f36a9dd3dd74c012277d438a3ace54e9aafd0
Michael <michaelmalis2@gmail.com>
2014-08-11 19:51:10 -0500

    It now works.

--- src/memoize.lisp
+++ src/memoize.lisp
@@ -11,9 +11,23 @@
 	    (= (gethash args cache)
 	       (apply f args))))))
 
+(def variable-names (args)
+	"Extracts the variable names from an argslist."
+	(map #'variable-name
+			 (rem [char= (char (symbol-name _) 0)
+									 #\&]
+						args)))
+
+(def variable-name (var)
+	"Extracts the variable name from a single element of an arglist."
+	(if (atom var)
+			var
+			(car var)))
+
 (mac defmemo (name args &body body)
   "Defines a memoized function"
-  `(do (defun ,name ())
+  `(do (defun ,name (,@args)
+				 (declare (ignorable ,@(variable-names args))))
 		   (= (symbol-function ',name)
 					(memo (fn ,args (block ,name ,@body))))
        ,(when (stringp (car body)) ; test for a documentation string
