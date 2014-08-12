fd15efce8885b02ea1802e4465081a1e92585203
Michael <michaelmalis2@gmail.com>
2014-08-12 18:42:37 -0500

    fixed indentation

--- src/macros.lisp
+++ src/macros.lisp
@@ -3,10 +3,10 @@
 (in-package :clamp)
 
 (mac w/uniq (names &body body)
-	"Binds each element in names (or names if it is just a symbol), with
+  "Binds each element in names (or names if it is just a symbol), with
    a unique symbol"
-	(if (consp names)
-			`(with ,(mappend (fn (n) `(,n (uniq (symbol-name ',n))))
-											 names)
-						 ,@body)
-			`(let ,names (uniq (symbol-name ',names)) ,@body)))
+  (if (consp names)
+      `(with ,(mappend (fn (n) `(,n (uniq (symbol-name ',n))))
+                       names)
+             ,@body)
+      `(let ,names (uniq (symbol-name ',names)) ,@body)))
