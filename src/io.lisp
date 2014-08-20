44b6ad5c1cb86e4339947b0bf3274f495c63a5b3
Michael <michaelmalis2@gmail.com>
2014-08-20 16:39:48 -0500

    Cleaned up Documentation

--- src/io.lisp
+++ src/io.lisp
@@ -1,28 +1,29 @@
-;;;; these are several utilities for performing i/o
+;;;; These are several utilities for performing i/o.
 
 (in-package :clamp)
 
 (mac w/infile (var file &body body)
-  "Binds var to the input stream from file and will automatically
-   close it"
+  "Binds VAR to the input stream created from FILE and will 
+   automatically close it when leaving the w/infile."
   `(w/file (,var ,file :direction :input) ,@body))
 
 (mac w/outfile (var file &body body)
-  "Binds var to the output stream from file and will
-   automatically close it. 
-   WARNING: This will delete the old file if it already exists"
+  "Binds VAR to the output stream created from FILE and will 
+   automatically close it when leaving w/outfile. 
+   WARNING: This will delete the old file if it already exists."
   `(w/file (,var ,file :direction :output :if-exists :supersede) ,@body))
 
 (mac w/appendfile (var file &body body)
-  "Same as w/outfile except appends to the file instead of superseding it"
+  "Equivalent to w/outfile but appends to FILE instead of superseding
+   it."
   `(w/file (,var ,file :direction :output :if-exists :append) ,@body))
 
 (def allchars (str)
-  "Returns a string of every char from the input stream, str."
+  "Returns a string of every char from the input stream, STR."
   (tostring
     (whiler c (readc :from str :eof nil) nil
       (writec c))))
 
 (def filechars (name)
-  "Returns a string of every char in the file named by 'name'."
+  "Returns a string of every char in the file NAME."
   (w/infile in name (allchars in)))
