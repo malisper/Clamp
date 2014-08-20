638a1999f2e49a1cca4a8b4dbd1731953573f53a
Michael <michaelmalis2@gmail.com>
2014-08-20 12:48:32 -0500

    Cleaned up Documentation.

--- src/base.lisp
+++ src/base.lisp
@@ -1,11 +1,11 @@
-;;;; these are some basic functions which need to be loaded first
+;;;; these are some basic utilities which need to be loaded first
 
 (in-package :clamp)
 
-;; this cannot be defined as an alias because then it would
-;; expand into #'(fn ..) which is an error
+;;; this cannot be defined as an alias because then it would
+;;; expand into #'(fn ..) which is an error
 (mac fn (args &body body)
-  "Equivalent to 'lambda' except this cannot be used as the name of
+  "Equivalent to lambda except this cannot be used as the name of
    of function (ie ((fn ..) ..))."
   `(lambda ,args ,@body))
 
@@ -17,20 +17,20 @@
       `(lambda (_) (,@(read-delimited-list #\] stream t)))))
 
 (def single (xs)
-  "A predicate for testing whether a list has only one element"
+  "Does this list have only one element?"
   (and (consp xs) (no (cdr xs))))
 
 (def pair (xs &optional (f #'list))
-  "Applies a function f to every two elements in xs"
+  "Applies f to every two elements of xs and collects the results."
   (cond ((no xs) '())
         ((single xs) (list (funcall f (car xs))))
         (:else (cons (funcall f (car xs) (cadr xs))
                      (pair (cddr xs) f)))))
 
 (mac if (&rest clauses)
-  "Cond but doesn't require parens for each clause"
+  "Equivalent to cond, but does not require parens parens around each
+   individual clause."
   ;; the if is needed to prevent warnings from occuring
   (cl:if (even (len clauses))
     `(cond ,@(pair clauses))
     `(cond ,@(pair (butlast clauses)) ,(cons t (last clauses))))) 
-
