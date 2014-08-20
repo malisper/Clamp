065375b6f8948a6299c0b50f8b8573d229354864
Michael <michaelmalis2@gmail.com>
2014-08-20 17:27:16 -0500

    Cleaned up Documentation

--- src/sort.lisp
+++ src/sort.lisp
@@ -1,17 +1,18 @@
-;;;; functions for specific kinds of sorting
+;;;; Functions for specific kinds of sorting.
 
 (in-package :clamp)
 
 (def compare (comparer scorer)
-  "Returns a function which compares its arguments score on scorer
-   with comparer. Generally should use the :key argument to other
-   procedures instead"
+  "Returns a function which compares its arguments score from SCORER
+   using COMPARER. Generally should use the :key argument to other
+   procedures instead of using compare."
   (fn (x y) (funcall comparer (funcall scorer x) (funcall scorer y))))
 
-;;; best can be modified easily to have it work an all sequences as
-;;; well as lists
+;;; It should be easy to modify best so that it works on all
+;;; sequences as well as lists.
 (def best (f xs &optional (key #'identity))
-  "Finds the first element of the list xs if it was sorted using f."
+  "Finds the first element of the list XS if it was sorted using 
+   the function F."
   (if (no xs)
       nil
       (ret wins (car xs)
@@ -22,15 +23,15 @@
                 (= wins elt
                    score elt-score))))))))
 
-(def bestn (n f xs)
-  "Returns the first n elements of the seq xs if it was sorted using 
-   f."
-  (firstn n (sort xs f)))
+(def bestn (n f seq)
+  "Returns a list containg the first N elements of SEQ if it was 
+   sorted using the function F."
+  (firstn n (sort f xs)))
 
 (def nsort (comparer sequence &optional (key #'identity))
-  "Destructively sorts the sequence using comparer."
+  "Destructively sorts SEQUENCE using COMPARER."
   (cl:sort sequence comparer :key key))
 
 (def sort (comparer sequence &optional (key #'identity))
-  "Non-destructively sorts sequence using comparer."
+  "Non-destructively sorts SEQUENCE using COMPARER."
   (nsort comparer (copy-seq sequence) key))
