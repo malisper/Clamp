44821f705c4fe96d76a0623711e2560a478d0fbd
Michael <michaelmalis2@gmail.com>
2014-08-20 15:05:28 -0500

    Cleaned up Documentation

--- src/fns.lisp
+++ src/fns.lisp
@@ -1,12 +1,14 @@
-;;;; These are macros which allow allow for different kinds of fns
+;;;; These are macros which allow for creation of functions.
 
 (in-package :clamp)
 
 (mac rfn (name parms &body body)
-  "Creates a recursive function which can refer to itself through name"
+  "Creates a recursive function which can refer to itself through 
+   NAME."
   `(labels ((,name ,parms ,@body))
      #',name))
 
 (mac afn (parms &body body)
-  "Creates a recursive function which can refer to itself through 'self'"
+  "Creates a recursive function which can refer to itself through 
+   the symbol 'self'."
   `(rfn self ,parms ,@body))
