91280192ad819bcad74616f14e0df4c37182492d
Michael <michaelmalis2@gmail.com>
2014-08-20 12:55:11 -0500

    Cleaned up Documentation

--- src/binding.lisp
+++ src/binding.lisp
@@ -3,26 +3,28 @@
 (in-package :clamp)
 
 (mac with (parms &body body)
-  "Destructuring-bind (let) but doesn't require parens for each binding"
+  "Equivalent to cl:let, but does not require parens around each
+   individual binding. This also allows for destructuring."
   (let* ((pparms (pair parms))
 	 (pats (map #'car  pparms))
 	 (vals (map #'cadr pparms)))
     `(destructuring-bind ,pats (list ,@vals) ,@body)))
 
 (mac let (var val &body body)
-  "With but with only one variable (no parens)"
+  "Equivalent to with, except binds only one variable."
   `(with (,var ,val) ,@body))
 
 (mac ret (var val &body body)
-  "Let but the result of the expression is the final value of the variable"
+  "Equivalent to clamp:let, but the result of a ret expression is the
+   final value of the variable being created."
   `(let ,var ,val ,@body ,var))
 
 (mac flet1 (name args fbody &body body)
-  "Flet but with only one binding (no parens)"
+  "Equivalent to flet, but only for one function definition."
   `(flet ((,name ,args ,fbody)) ,@body))
 
 (mac withs (parms &body body)
-  "With analog for let* but doesn't require parens around each binding"
+  "Equivalent to let*, but allows destructuring."
   (if (no parms)
       `(do ,@body)
       `(let ,(car parms) ,(cadr parms)
