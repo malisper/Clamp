476d4754c3004c40e5ff66886a264580d48d4991
Michael <michaelmalis2@gmail.com>
2014-08-25 20:12:49 -0500

    Defalias now allows a docstring.
    This is useful for defining interfaces to simple structures such as
    one represented as a list. This way it is possible to change the doc
    string to the accessor.

--- src/defalias.lisp
+++ src/defalias.lisp
@@ -7,31 +7,31 @@
   "Is this a macro?"
   (and (symbolp x) (macro-function x)))
 
-(defun make-macro (new old)
+(defun make-macro (new old &optional doc)
   "Generates the code for making the macros NEW and OLD equivalent."
   (cl:let ((rest (gensym "REST")))
     `(progn
        (defmacro ,new (&rest ,rest) `(,',old ,@,rest))
-       (setf (documentation ',new 'function)
-             (documentation ',old 'function)
-             (macro-function ',new)
-             (macro-function ',old)))))
+       (setf (macro-function ',new)
+             (macro-function ',old)
+             (documentation ',new 'function)
+             ,(or doc `(documentation ',old 'function))))))
 
 (defun fnp (x)
   "Is this a function?"
   (and (symbolp x) (symbol-function x)))
 
-(defun make-fn (new old)
+(defun make-fn (new old &optional doc)
   "Generates the code for making NEW and OLD the same function."
   ;; Copying the compiler macro can lead to weird behavior because it
   ;; might rely on the original name of the function.
   (cl:let ((args (gensym "ARGS")))
     `(progn
        (defun ,new (&rest ,args) (apply #',old ,args))
-       (setf (documentation ',new 'function)
-             (documentation ',old 'function)
-             (symbol-function ',new)
-             (symbol-function ',old)))))
+       (setf (symbol-function ',new)
+             (symbol-function ',old)
+             (documentation ',new 'function)
+             ,(or doc `(documentation ',old 'function))))))
 
 (defun make-special-macro (new old)
   "Generates the code to create a macro NEW which expands into a use 
@@ -40,15 +40,15 @@
     `(defmacro ,new (&rest ,rest)
        `(,',old ,@,rest))))
 
-(defmacro defalias (new old)
+(defmacro defalias (new old &optional doc)
   "Makes a use of NEW the equivalent to a use of OLD. Works on
    functions, macros, and (most) special forms."
   (cond ((special-operator-p old)
          (make-special-macro new old))
         ((macrop old)
-         (make-macro new old))
+         (make-macro new old doc))
         ((fnp old)
-         (make-fn new old))
+         (make-fn new old doc))
         (:else
          (error "Don't know what to do for object ~A of type ~A"
                 old (type-of old)))))
