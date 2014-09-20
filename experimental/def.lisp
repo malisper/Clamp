bfab6f07a97775f7ed3588a7cb3ea44f4034a0de
Michael <michaelmalis2@gmail.com>
2014-09-20 14:11:43 -0500

    Added documentation to def about using ssyntax and args lists.

--- experimental/def.lisp
+++ experimental/def.lisp
@@ -3,6 +3,10 @@
 (mac def (name args &body body)
   "Same as clamp:defun but allows ?, !, and argument destrucuring."
   `(w/ssyntax
+    ;; The arguments list has to be expanded manually. Otherwise
+    ;; symbols such as ! would be considered ssyntax. The alternative
+    ;; would be to put a couple of "patches" into the implementation of
+    ;; ssyntax which would ignore those cases.
      ,(mvb (new-args alist) (parse-args args)
         (if (null alist)
             `(defun ,name ,new-args ,@body)
