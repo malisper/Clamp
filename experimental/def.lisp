82fe3023137fdc2890c8db90f3bb33ccff103441
Michael <michaelmalis2@gmail.com>
2014-10-03 08:55:55 -0500

    dbind -> let.

--- experimental/def.lisp
+++ experimental/def.lisp
@@ -11,5 +11,5 @@
         (if (null alist)
             `(defun ,name ,new-args ,@body)
             `(defun ,name ,new-args
-               (dbind ,(map #'cadr alist) (list ,@(map #'car alist))
+               (let ,(map #'cadr alist) (list ,@(map #'car alist))
                  ,@body))))))
