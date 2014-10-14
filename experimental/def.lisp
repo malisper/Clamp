9757330800b3d696f26cc0b461ae59fd854ef6ee
Michael <michaelmalis2@gmail.com>
2014-10-13 20:05:16 -0500

    Simplified experimental definitions special forms.
    I did this by defining a new macro, defexperimental, which create an
    experimental defining special form when given a regular one.

--- experimental/def.lisp
+++ experimental/def.lisp
@@ -1,46 +1,18 @@
 (in-package :experimental)
 
-(defmacro def (name args &body body)
-  "Same as clamp:def but allows ?, !, ssyntax, and argument 
-   destrucuring."
-  `(w/ssyntax
-    ;; The arguments list has to be expanded manually. Otherwise
-    ;; symbols such as ! would be considered ssyntax. The alternative
-    ;; would be to put a couple of "patches" into the implementation of
-    ;; ssyntax which would ignore those cases.
-     ,(mvb (new-args alist) (parse-args args)
-        (if (null alist)
-            `(defun ,name ,new-args ,@body)
-            `(defun ,name ,new-args
-               (let ,(map #'cadr alist) (list ,@(map #'car alist))
-                 ,@body))))))
+(defmacro defexperimental (old new)
+  "Defines a definition special form, NEW, which is the same as the
+   definition special form, OLD, except it allows for ssyntax,
+   and the new argument lists."
+  `(defmacro ,new (name args &body body)
+     `(w/ssyntax
+        ,(mvb (new-args alist) (parse-args args)
+           (if (null alist)
+               `(,',old ,name ,new-args ,@body)
+               `(,',old ,name ,new-args ,@body
+                  (let ,(map #'cadr alist) (list ,@(map #'car alist))
+                    ,@body)))))))
 
-(defmacro defmemo (name args &body body)
-  "Same as clamp:defmemo but allows ?, !, ssyntax, and argument 
-   destrucuring."
-  `(w/ssyntax
-    ;; The arguments list has to be expanded manually. Otherwise
-    ;; symbols such as ! would be considered ssyntax. The alternative
-    ;; would be to put a couple of "patches" into the implementation of
-    ;; ssyntax which would ignore those cases.
-     ,(mvb (new-args alist) (parse-args args)
-        (if (null alist)
-            `(clamp:defmemo ,name ,new-args ,@body)
-            `(clamp:defmemo ,name ,new-args
-               (let ,(map #'cadr alist) (list ,@(map #'car alist))
-                 ,@body))))))
-
-(defmacro mac (name args &body body)
-  "Same as clamp:mac but allows ?, !, ssyntax, and argument 
-   destrucuring."
-  `(w/ssyntax
-    ;; The arguments list has to be expanded manually. Otherwise
-    ;; symbols such as ! would be considered ssyntax. The alternative
-    ;; would be to put a couple of "patches" into the implementation of
-    ;; ssyntax which would ignore those cases.
-     ,(mvb (new-args alist) (parse-args args)
-        (if (null alist)
-            `(defmacro ,name ,new-args ,@body)
-            `(defmacro ,name ,new-args
-               (let ,(map #'cadr alist) (list ,@(map #'car alist))
-                 ,@body))))))
+(defexperimental def clamp:def)
+(defexperimental defmemo clamp:defmemo)
+(defexperimental mac clamp:mac)
