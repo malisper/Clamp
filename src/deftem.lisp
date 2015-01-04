e52ecda3677946543cc27010ffbc93310474ab7b
Michael Malis <michaelmalis2@gmail.com>
2015-01-04 11:15:20 -0600

    Added options to deftem.
    These include the :conc-name, :predicate, and :constructor options.

--- src/deftem.lisp
+++ src/deftem.lisp
@@ -4,28 +4,35 @@
 
 (in-package :clamp)
 
-(mac deftem (name &rest slots)
-  (let names (map #'carif slots)
+(mac deftem (name-and-options &rest slots)
+  (withs (slot-names (map #'carif slots)
+	  name (carif name-and-options)
+	  options (if (listp name-and-options) (cdr name-and-options) '())
+	  constructor-name (or2 (alref options :constructor) (symb 'make- name))
+	  predicate-name (or2 (alref options :predicate) (symb name '-p))
+	  conc-name (or2 (alref options :conc-name) (symb name '-)))
     `(do (defclass ,name ()
 	   ,(mapeach s slots
 	      (let (slot-name &optional initform) (mklist s)
-		`(,slot-name :accessor ,(symb name '- slot-name)
+		`(,slot-name :accessor ,(if conc-name (symb conc-name slot-name) slot-name)
 			     :initarg  ,(intern (mkstr slot-name) :keyword)
 			     :initform ,initform))))
-	   
-	 (def ,(symb name '-p) (object)
-	   ,(tostring (prf "Is OBJECT of type ~(~A~)?" name))
-	   (typep object ',name))
+	 ,(when predicate-name  
+	    `(def ,predicate-name (object)
+	       ,(tostring (prf "Is OBJECT of type ~(~A~)?" name))
+	       (typep object ',name)))
 	  
-         ,(w/uniq args
-	    `(def ,(symb 'make- name) (&rest ,args &key ,@names)
-	       ,(tostring (prf "Create an object of type ~(~A~)." name))
-	       (declare (ignore ,@names))
-	       (apply #'make-instance ',name ,args)))
+         ,(when constructor-name
+	    (w/uniq args
+	      `(def ,constructor-name (&rest ,args &key ,@slot-names)
+		 ,(tostring (prf "Create an object of type ~(~A~)." name))
+		 (declare (ignore ,@slot-names))
+		 (apply #'make-instance ',name ,args))))
 	  
 	  (defmethod print-object ((obj ,name) stream)
 	    ,(tostring (prf "Print an object of type ~(~A~)." name))
 	    (print-unreadable-object (obj stream :type t)
-	      (with-slots ,names obj
+	      (with-slots ,slot-names obj
 		(format stream "~{:~A ~A~^ ~}"
-		        (list ,@(mappendeach n names `(',n ,n))))))))))
+		        (list ,@(mappendeach n slot-names `(',n ,n)))))))
+          ',name)))
