999268f66b860789f33c5c44ce0563a4f6f9805a
Michael Malis <michaelmalis2@gmail.com>
2015-01-21 15:05:27 -0600

    Added primitive support for superclasses.
    There are some problems around how the print-object and the
    constructor are generated since they only use the slots of the current
    template.

--- src/deftem.lisp
+++ src/deftem.lisp
@@ -11,25 +11,26 @@
 	  constructor-name (or2 (alref options :constructor) (symb 'make- name))
 	  predicate-name (or2 (alref options :predicate) (symb name '-p))
 	  conc-name (or2 (alref options :conc-name) (symb name '-))
-          printer-name (alref options :print-object))
-    `(do (defclass ,name ()
+          printer-name (alref options :print-object)
+          direct-superclasses (cdr (assoc :include options)))
+    `(do (defclass ,name ,direct-superclasses
 	   ,(mapeach s slots
 	      (let (slot-name &optional initform) (mklist s)
 		`(,slot-name :accessor ,(if conc-name (symb conc-name slot-name) slot-name)
 			     :initarg  ,(intern (mkstr slot-name) :keyword)
 			     :initform ,initform))))
-	 ,(when predicate-name  
+	 ,(when predicate-name
 	    `(def ,predicate-name (object)
 	       ,(tostring (prf "Is OBJECT of type ~(~A~)?" name))
 	       (typep object ',name)))
-	  
+
          ,(when constructor-name
 	    (w/uniq args
 	      `(def ,constructor-name (&rest ,args &key ,@slot-names)
 		 ,(tostring (prf "Create an object of type ~(~A~)." name))
 		 (declare (ignore ,@slot-names))
 		 (apply #'make-instance ',name ,args))))
-	  
+
 	  (defmethod print-object ((obj ,name) stream)
 	    ,(tostring (prf "Print an object of type ~(~A~)." name))
 	    ,(if printer-name
