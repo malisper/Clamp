fb702a218f656c4459b4042e2e4168432b2ec081
Michael Malis <michaelmalis2@gmail.com>
2015-07-03 11:50:16 -0400

    Fixed spelling of linearlize.

--- src/package.lisp
+++ src/package.lisp
@@ -43,7 +43,7 @@
    ;; From list.
    :mklist :dotted :proper :range :firstn :last :flat :len< :len>
    :n-of :drain :split :group :caris :carif :consif :conswhen :cars
-   :cdrs :get :trav :intersperse :linearalize
+   :cdrs :get :trav :intersperse :linearlize
 
    ;; From macros.
    :w/uniq :mkstr :symb :check-len :once-only
